f376e0181365220054854881573cf1242fe3a7c9
Carlos Andres Chavarria Quiros <carlos.chavarriaquiros@ucr.ac.cr>
2025-09-05 15:37:14 -0600

    pimplementacion de lector de csv terminado

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,44 @@
 #include <ctime>
 #include <chrono>
 #include "mergeSort.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include "mergeSort.h"
 using namespace std;
 using namespace std::chrono;
 
 int main() {
-    int n;
+    
+        string filename;
+    cout << "Ingrese el nombre del archivo CSV (ej: datos.csv): ";
+    cin >> filename;
 
-    cout << "Ingrese la cantidad de elementos a ordenar: ";
-    cin >> n;
+    ifstream file(filename);
+    if (!file.is_open()) {
+        cerr << "Error al abrir el archivo: " << filename << endl;
+        return 1;
+    }
+
+    vector<int> data;
+    string line;
+
+    while (getline(file, line)) {
+        stringstream ss(line);
+        string token;
+        while (getline(ss, token, ',')) {
+            try {
+                int value = stoi(token);
+                data.push_back(value);
+            } catch (...) {
+                // Ignorar encabezados u otros errores
+            }
+        }
+    }
+
+    file.close();
+    int n= data.size();
 
     // Crear arreglo dinámico
     int* arr = new int[n];
@@ -20,7 +50,7 @@ int main() {
 
     // Llenar el arreglo con valores aleatorios
     for (int i = 0; i < n; i++) {
-        arr[i] = rand() % 1000; // valores entre 0 y 999
+        arr[i] = data[i]; // valores entre 0 y 999
     }
 
 
@@ -32,13 +62,13 @@ int main() {
 
     // Tomar tiempo de fin
     auto stop = high_resolution_clock::now();
-    auto duration = duration_cast<microseconds>(stop - start);
+    auto duration = duration_cast<nanoseconds>(stop - start);
 
     // Mostrar arreglo ordenado  y tiempo de ejecución  
         cout << "\nArreglo ordenado:\n";
         printArray(arr, n);
 
-    cout << "\nTiempo de ejecución: " << duration.count() << " milisegundos\n";
+    cout << "\nTiempo de ejecución: " << duration.count() << " nanosegundos\n";
 
     // Liberar memoria
     delete[] arr;
